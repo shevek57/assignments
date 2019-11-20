ffd576d91adfa1457745c9e3ff78490a30632fd9
Katz <ikatz@ETS.ORG>
2019-11-20 16:58:27 -0500

    :tada: React assignment 1

---
+++ react-assignment-1/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h3>This is the footer</h3>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ react-assignment-1/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>First</li>
+                <li>Second</li>
+                <li>Third</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
