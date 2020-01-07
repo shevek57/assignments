c56c597c2e0a4a4b3d6f0d921e11810e3ec01de8
Katz <ikatz@ETS.ORG>
2020-01-07 17:19:21 -0500

    Phase 7 commit

--- TODO Phase 6 and 7/src/TodoItem.js
+++ TODO Phase 6 and 7/src/TodoItem.js
@@ -1,13 +1,14 @@
 import React from "react"
 
 function TodoItem({item, checkClicked}) {
+    const checkedStyle = {color: "grey", textDecoration: "line-through"}
     return (
         <div className="todo-item">
             <form>
             <input type="checkbox" 
                     checked={item.completed} 
                     onChange={ checkClicked } />
-            <p>{item.text}</p>
+            <p style={item.completed ? checkedStyle: null}>{item.text}</p> {/* for some reason, "item.completed && checkedStyle does not work" */}
             </form>
         </div>
     )
